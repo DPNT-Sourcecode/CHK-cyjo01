7e81d4172e80902df6a358303b8a5e3588e6870c
sbx_user1051 <sbx_user1051@169.254.17.229>
2020-02-04 20:23:53 +0000

    Tue Feb 04 20:23:53 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,49 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        final String[] skuCodes = skus.split(",");
+        final Map<String, Integer> skuMap = new HashMap<String, Integer>();
+        for (String key : skuCodes) {
+            if (skuMap.containsKey(key)) {
+                skuMap.put(key, skuMap.get(key) + 1);
+            }
+            else {
+                skuMap.put(key, 1);
+            }
+        }
+
+        System.out.println(skuMap);
+
+        int totalValue = 0;
+
+        for (Map.Entry<String,Integer> entry : skuMap.entrySet())  {
+
+            switch (entry.getKey()) {
+                case "D":
+                    totalValue+=15;
+                    break;
+                case "C":
+                    totalValue+=20;
+                    break;
+                case "B":
+                    totalValue+=(int) Math.floor( entry.getValue() / 2 * 45 + entry.getValue() % 2 * 30);
+                    break;
+                case "A":
+                    totalValue+=(int) Math.floor( entry.getValue() / 3 * 130 + entry.getValue() % 3 * 50);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return totalValue;
+
     }
 }
+
